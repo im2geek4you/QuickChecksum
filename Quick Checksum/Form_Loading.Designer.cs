65b1b611ea801bc3de7fb7f6d29d931b14dc13f6
im2geek4you <im2geek4you@gmail.com>
2017-05-17 01:26:40 +0100

    First commit. Lots of stuff missing yet.

---
+++ Quick Checksum/Form_Loading.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Quick_Checksum
+{
+    partial class Form_Loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Loading));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_Files = new System.Windows.Forms.Label();
+            this.backgroundWorkerChecksum = new System.ComponentModel.BackgroundWorker();
+            this.labelClose = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label_Files
+            // 
+            resources.ApplyResources(this.label_Files, "label_Files");
+            this.label_Files.Name = "label_Files";
+            // 
+            // backgroundWorkerChecksum
+            // 
+            this.backgroundWorkerChecksum.WorkerReportsProgress = true;
+            this.backgroundWorkerChecksum.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerChecksum_DoWork);
+            this.backgroundWorkerChecksum.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerChecksum_ProgressChanged);
+            this.backgroundWorkerChecksum.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerChecksum_RunWorkerCompleted);
+            // 
+            // labelClose
+            // 
+            resources.ApplyResources(this.labelClose, "labelClose");
+            this.labelClose.Name = "labelClose";
+            this.labelClose.Click += new System.EventHandler(this.labelClose_Click);
+            // 
+            // Form_Loading
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelClose);
+            this.Controls.Add(this.label_Files);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Loading";
+            this.Load += new System.EventHandler(this.Form_Loading_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_Files;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerChecksum;
+        private System.Windows.Forms.Label labelClose;
+    }
+}
+
---
+++ Quick Checksum/Form_Loading.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_Checksum
+{
+    public partial class Form_Loading : Form
+    {
+        string md5sum;
+        string sha1sum;
+        string sha256sum;
+        string filename = string.Empty;
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+
+        public Form_Loading()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+
+        private void Form_Loading_Load(object sender, EventArgs e)
+        {
+            
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                label_Files.Text = "File: " + args[1];
+                filename = args[1];
+
+                backgroundWorkerChecksum.RunWorkerAsync();
+
+            }
+
+        }
+
+        private void backgroundWorkerChecksum_DoWork(object sender, DoWorkEventArgs e)
+        {
+            backgroundWorkerChecksum.ReportProgress(99);
+
+
+            using (var stream = File.OpenRead(filename))
+            {
+                    
+                using (var md5 = MD5.Create())
+                {
+                    md5sum = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+
+                using (var sha1 = SHA1.Create())
+                {
+                    sha1sum = BitConverter.ToString(sha1.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+                
+                using (var sha256 = SHA256.Create())
+                {
+                    sha256sum = BitConverter.ToString(sha256.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+                
+            }
+            
+        }
+
+        private void backgroundWorkerChecksum_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Hide();
+            Form_SingleFile frmSingleFile = new Form_SingleFile(filename, md5sum, sha1sum, sha256sum);
+            frmSingleFile.Closed += (s, args) => this.Close();
+            frmSingleFile.Show();
+
+        }
+
+        private void backgroundWorkerChecksum_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
